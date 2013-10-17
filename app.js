35ac10590d7a7e24f0a46759be717dfdc3f4be29
Tim Pillard <pillard@intechinfo.fr>
2013-10-17 19:09:20 +0100

    Enabling all origins for now

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var app = express();
 var allowedHosts = ['http://localhost:3000', 'http://tapastreet-facebook.herokuapp.com'];
 
 var allowCrossDomain = function(req, res, next) {
-
-    if(allowedHosts.indexOf(req.headers.origin) !== -1) {
+    console.log(req.headers.origin);
+    if(allowedHosts.indexOf(req.headers.origin) !== -1 || true) {
         res.header('Access-Control-Allow-Credentials', true);
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
         res.header('Access-Control-Allow-Methods', 'GET');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
         // intercept OPTIONS method
