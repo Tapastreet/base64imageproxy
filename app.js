d88c1609129d8b0a581b96dd646346b1d3b64474
Your Name <pillard@intechinfo.fr>
2013-10-19 23:47:22 +0100

    test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require("express"),
     app = express(),
     allowedHosts = ['http://tapastreet-facebook.herokuapp.com', 'https://tapastreet-facebook.herokuapp.com'],
     allowCrossDomain = function(req, res, next) {
-    if(allowedHosts.indexOf(req.headers.origin) !== -1) {
+    if(allowedHosts.indexOf(req.headers.origin) !== -1 || true) {
         res.header('Access-Control-Allow-Origin', req.headers.origin);
         res.header('Access-Control-Allow-Methods', 'GET');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
